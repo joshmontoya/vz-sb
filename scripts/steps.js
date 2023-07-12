859b7391786d8eeb5cf3f8960f13bf864a172fd0
Josh Montoya <84519241+joshmontoya@users.noreply.github.com>
2023-07-12 10:54:27 -0700

    Delete scripts directory

--- scripts/steps.js
+++
@@ -1,8 +0,0 @@
-
-let scenarios = JSON.parse(localStorage.scenarioData);
-
-let outputSteps = [];
-let outputStepsAllScenarios = [];
-let manualOutputSteps = [];
-let manualOutputStepsAllScenarios = [];
-let fieldFuncCount = 0;
